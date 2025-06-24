6f85a23d9593aeb7d09facac0a5e51816ed8e380
Geovanny Mendoza <me@geovannycode.com>
2025-06-24 12:00:00 -0500

    Commit retroactivo

--- src/main/java/org/gmendoza/quarkus/starting/BookResource.java
+++ src/main/java/org/gmendoza/quarkus/starting/BookResource.java
@@ -5,12 +5,18 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
+import java.util.List;
+
 @Path("/api/books")
+@Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello RESTEasy";
+    public List<Book> getBooks() {
+        return List.of(
+                new Book(1,"Understanding Kotlin", "Geovanny", 2025, "IT" ),
+                new Book(2,"Understanding Quarkus", "Omar", 2024, "IT" ),
+                new Book(3,"Understanding Java", "Elena", 2023, "IT" )
+        );
     }
 }
