06081b0ee625ce74556e5615cf9d551c9dfa1ded
Geovanny Mendoza <me@geovannycode.com>
2025-06-27 19:30:18 -0500

    Add repository and update resource

---
+++ src/main/java/org/gmendoza/quarkus/starting/BookRepository.java
@@ -0,0 +1,33 @@
+package org.gmendoza.quarkus.starting;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+import java.util.List;
+import java.util.Optional;
+
+@ApplicationScoped
+public class BookRepository {
+
+
+    public List<Book> getAllBooks() {
+        return List.of(
+                new Book(1,"Understanding Kotlin", "Geovanny Mendoza", 2025, "IT" ),
+                new Book(2,"Understanding Quarkus", "Omar Berroteran", 2024, "IT" ),
+                new Book(3,"Understanding Java", "Elena Aguirre", 2023, "IT" ),
+                new Book(4,"Effective Java in Action", "Gerardo Nuñez", 2022, "IT" )
+        );
+    }
+
+    public Optional<Book> getBook(@PathParam("id") int id) {
+        return getAllBooks().stream().filter(book -> book.id==id).findFirst();
+    }
+
+    
+
+
+}
--- src/main/java/org/gmendoza/quarkus/starting/BookResource.java
+++ src/main/java/org/gmendoza/quarkus/starting/BookResource.java
@@ -1,10 +1,12 @@
 package org.gmendoza.quarkus.starting;
 
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.jboss.logging.Logger;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,23 +15,25 @@ import java.util.Optional;
 @Produces(MediaType.APPLICATION_JSON)
 public class BookResource {
 
+    @Inject
+    BookRepository bookRepository;
+    @Inject
+    Logger logger;
+
     @GET
     public List<Book> getAllBooks() {
-        return List.of(
-                new Book(1,"Understanding Kotlin", "Geovanny Mendoza", 2025, "IT" ),
-                new Book(2,"Understanding Quarkus", "Omar Berroteran", 2024, "IT" ),
-                new Book(3,"Understanding Java", "Elena Aguirre", 2023, "IT" ),
-                new Book(4,"Effective Java in Action", "Gerardo Nuñez", 2022, "IT" )
-        );
+        logger.info("Returns all the books");
+        return bookRepository.getAllBooks();
     }
 
     @GET
     @Path("{id}")
     public Optional<Book> getBook(@PathParam("id") int id) {
-        return getAllBooks().stream().filter(book -> book.id==id).findFirst();
+        logger.info("Returns book with id " + id);
+        return bookRepository.getBook(id);
     }
 
-    
+
 
 
 }
