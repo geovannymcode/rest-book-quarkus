27034ea85b73b59ee89649fbe73c42c8f777ca4f
Geovanny Mendoza <me@geovannycode.com>
2025-07-08 23:09:19 -0500

    Add format code

--- src/main/java/org/gmendoza/quarkus/starting/Book.java
+++ src/main/java/org/gmendoza/quarkus/starting/Book.java
@@ -2,17 +2,17 @@ package org.gmendoza.quarkus.starting;
 
 public class Book {
 
-    public int id;
-    public String title;
-    public String author;
-    public int yearOfPublication;
-    public String genre;
+  public int id;
+  public String title;
+  public String author;
+  public int yearOfPublication;
+  public String genre;
 
-    public Book(int id, String title, String author, int yearOfPublication, String genre) {
-        this.id = id;
-        this.title = title;
-        this.author = author;
-        this.yearOfPublication = yearOfPublication;
-        this.genre = genre;
-    }
+  public Book(int id, String title, String author, int yearOfPublication, String genre) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+    this.yearOfPublication = yearOfPublication;
+    this.genre = genre;
+  }
 }
