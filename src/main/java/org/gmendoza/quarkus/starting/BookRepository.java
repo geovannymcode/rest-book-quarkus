2549940150865ca02e00353bbfacbd798511e35a
Geovanny Mendoza <me@geovannycode.com>
2025-07-08 23:09:27 -0500

    Add code format

--- src/main/java/org/gmendoza/quarkus/starting/BookRepository.java
+++ src/main/java/org/gmendoza/quarkus/starting/BookRepository.java
@@ -1,33 +1,26 @@
 package org.gmendoza.quarkus.starting;
 
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
-import jakarta.ws.rs.core.MediaType;
-import org.eclipse.microprofile.config.inject.ConfigProperty;
-
 import java.util.List;
 import java.util.Optional;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
 
 @ApplicationScoped
 public class BookRepository {
 
-    @ConfigProperty(name = "books.genre", defaultValue = "Sci-Fi")
-    String genre;
+  @ConfigProperty(name = "books.genre", defaultValue = "Sci-Fi")
+  String genre;
 
-    public List<Book> getAllBooks() {
-        return List.of(
-                new Book(1,"Understanding Kotlin", "Geovanny Mendoza", 2025, genre ),
-                new Book(2,"Understanding Quarkus", "Omar Berroteran", 2024, genre ),
-                new Book(3,"Understanding Java", "Elena Aguirre", 2023, genre ),
-                new Book(4,"Effective Java in Action", "Gerardo Nuñez", 2022, genre )
-        );
-    }
+  public List<Book> getAllBooks() {
+    return List.of(
+        new Book(1, "Understanding Kotlin", "Geovanny Mendoza", 2025, genre),
+        new Book(2, "Understanding Quarkus", "Omar Berroteran", 2024, genre),
+        new Book(3, "Understanding Java", "Elena Aguirre", 2023, genre),
+        new Book(4, "Effective Java in Action", "Gerardo Nuñez", 2022, genre));
+  }
 
-    public Optional<Book> getBook(@PathParam("id") int id) {
-        return getAllBooks().stream().filter(book -> book.id==id).findFirst();
-    }
-
+  public Optional<Book> getBook(@PathParam("id") int id) {
+    return getAllBooks().stream().filter(book -> book.id == id).findFirst();
+  }
 }
